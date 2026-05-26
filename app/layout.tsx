484f411c92f1269e2ce285772cd99f5b6bc6f5ec
huyndse161632 <huyndse161632@fpt.edu.vn>
2026-05-26 13:19:45 +0700

    Change background again

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="vi" className={inter.variable}>
-      <body className="min-h-screen bg-white antialiased">
+      <body className="min-h-screen antialiased">
         {children}
       </body>
     </html>
